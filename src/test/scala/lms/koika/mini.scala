5a88d680cdda62d76630f0daaeb194b98310a9ec
Nada Amin <namin@seas.harvard.edu>
2022-04-14 16:00:10 -0400

    init

---
+++ src/test/scala/lms/koika/mini.scala
@@ -0,0 +1,19 @@
+package lms.koika
+
+import lms.core.stub._
+import lms.core.virtualize
+import lms.macros.SourceContext
+
+@virtualize
+@virtualize
+class MiniTest extends TutorialFunSuite {
+  val under = "mini_"
+
+  test("mini stub") {
+    val snippet = new DslDriver[Int,Int] {
+      def f(a: Rep[Int]): Rep[Int] = if (a==0) 0 else (a+1)
+      def snippet(a: Rep[Int]) = f(f(f(a)))
+    }
+    exec("stub", snippet.code)
+  }
+}
